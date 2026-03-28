a8528f5eb63e9b13c9a9b182721271d5562da2d6
trab <btraore25@yahoo.com>
2026-03-28 14:06:27 +0100

    Add a Header

--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -3,6 +3,16 @@ export const fr = {
     homepage: {
         description: "Site web de Spicy Food.",
     },
+    header: {
+        navbarStart: {
+            reserve: "Réserver",
+            news: "Actualités",
+            contact: "Contactez-nous"
+        },
+        navbarCenter: {
+            logoText: "Spicy Food"
+        },
+    },
     notFoundPage: {
         title: "404 Page non trouvée !",
         callToAction: "Retourner à l'accueil",
