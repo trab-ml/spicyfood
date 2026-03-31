2b4bba034af65ccb6fb4a29ac17c8428aa6cfe68
trab <btraore25@yahoo.com>
2026-03-31 15:55:47 +0200

    Add image to the first section of homepage

--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -8,7 +8,20 @@ export const fr = {
             partThree: "Perfection",
         },
         label: "AUTHENTIC FLAVORS",
-        description: "A curated journey through West African markets and Spanish galleries, brought to the heart of Lille.", 
+        description: "A curated journey through West African markets and Spanish galleries, brought to the heart of Lille.",
+        secretSignature: {
+            heading: "THE JUICE",
+            subheading: "Our Secret Signature",
+            description: "Not just a sauce, but the soul of our kitchen. A slow-simmered vegetable meat base that bridges continents.",
+            specialityOne: {
+                name: "Process",
+                description: "12-Hour Simmer"
+            },
+            specialityTwo: {
+                name: "Essence",
+                description: "Spicy Roots"
+            },
+        }
     },
     header: {
         navbarStart: {
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,12 +10,24 @@
                 $t("homepage.headingOne.partThree")
             }}</span>
         </h1>
-        <span class="text-lg text-(--light-dark)">{{ $t("homepage.description") }}</span>
+        <span class="text-lg text-(--light-dark)">{{
+            $t("homepage.description")
+        }}</span>
         <span class="flex w-full justify-between mt-5 text-(--primary)"
             ><span class="inline w-3/6 mt-2.5 border-t"></span>
             <silverware-fork-knife class="inline mx-4 text-(--primary)" />
-            <span class="inline w-3/6 mt-2.5 border-t"></span></span
-        >
+            <span class="inline w-3/6 mt-2.5 border-t"></span
+        ></span>
+        <div class="relative h-96 my-6 overflow-x-hidden overflow-y-hidden drop-shadow-xl/50 drop-shadow-primary">
+            <img
+                class="ml-8 rounded-l-[6rem] object-cover w-full h-full"
+                src="/spicy-african-dish.png"
+                alt="Gourmet spicy African dish with plantains and vibrant garnish"
+            />
+        </div>
+    </section>
+    <section>
+        <h2>{{ $t("homepage.secretSignature.heading") }}</h2>
     </section>
 </template>
 
