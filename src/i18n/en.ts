f094e170b1e89274e62cc2931280f39cb5f650fe
trab <btraore25@yahoo.com>
2026-04-05 20:33:20 +0200

    Get i18n files clean

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,11 +1,115 @@
 export const en = {
-  abbreviation: "En",
-  homepage: {
-    description: "Spicy Food website.",
-  },
-  notFoundPage: {
-    title: "404 Page not found !",
-    callToAction: "Go back home",
-    description: "Page not found.",
-  },
+    abbreviation: "En",
+    header: {
+        navbarStart: {
+            reserve: "Reserve",
+            news: "News",
+            contact: "Being in touch",
+        },
+        navbarCenter: {
+            logoText: "Spicy Food",
+        },
+    },
+    homepage: {
+        metaDescription: "Website of Spicy Food.",
+        headingOne: {
+            partOne: "Spiced",
+            partTwo: "to",
+            partThree: "Perfection",
+        },
+        label: "AUTHENTIC FLAVORS",
+        description:
+            "A curated journey through West African markets and Spanish galleries, brought to the heart of Lille.",
+        secretSignature: {
+            heading: "THE JUICE",
+            subheading: "OUR SECRET SIGNATURE",
+            description:
+                '"Not just a sauce, but the soul of our kitchen. A slow-simmered vegetable meat base that bridges continents."',
+            specialityOne: {
+                name: "PROCESS",
+                description: "12-Hour Simmer",
+            },
+            specialityTwo: {
+                name: "ESSENCE",
+                description: "Spiced Roots",
+            },
+        },
+        menu: {
+            heading: "THE MENU",
+            mostPopulars: "MOST POPULARS",
+            dishOne: {
+                name: "Attiéké",
+                description: "The legendary Cassava couscous.",
+                label: "West Africa",
+                imgSrc: "/attieke-plantains-poisson.png",
+                imgAlt: "attiéké plantains fish",
+            },
+            dishTwo: {
+                name: "Plantain's Burger",
+                description:
+                    "The amazing Plantain's Burger (greater bananas:).",
+                label: "Greater Antilles",
+                imgSrc: "/burger-bananes-plantains.png",
+                imgAlt: "burger plantains",
+            },
+            dishThree: {
+                name: "Tchèbe",
+                description: "The legendary Thieboudienne.",
+                label: "West Africa",
+                imgSrc: "/tcheboudiene-poisson.png",
+                imgAlt: "tcheboudiene fish",
+            },
+            dishFour: {
+                name: "Mafé",
+                description: "The phenomenal Mafé, made from peanut paste.",
+                label: "West Africa",
+                imgSrc: "/sauce-mafe-poulet.png",
+                imgAlt: "sauce mafé chicken",
+            },
+        },
+        spiceRoots: {
+            heading: "SPICE ROOTS",
+            subheading: "Mapping the path of flavor from origin to plate.",
+            dishes: [
+                {
+                    name: "Berberé",
+                    description: "ETHIOPIA VIA SPAIN",
+                    imgSrc: "/berbere.png",
+                    imgAlt: "The legendary Berberé",
+                },
+                {
+                    name: "Pimenton",
+                    description: "MURCIA HIGHLANDS",
+                    imgSrc: "/bell-pepper.png",
+                    imgAlt: "bell pepper",
+                },
+                {
+                    name: "Turmeric",
+                    description: "LAGOS MARKETS",
+                    imgSrc: "/turmeric.png",
+                    imgAlt: "turmeric",
+                },
+            ],
+        },
+        callOfAction: {
+            heading: "CRAFT YOUR CHRONICLE",
+            bookMsg: "BOOK A TABLE IN LILLE",
+            description: "Limited seating. Infinite flavor.",
+        },
+    },
+    footer: {
+        email: "contact{'@'}spicyfood.fr",
+        creator: {
+            name: "Made by trab-ml",
+            contact: "https://github.com/trab-ml",
+        },
+        policy: "PRIVACY POLICY",
+        privacy: "TERMS OF PRIVACY",
+        copyright: "© Spicy Food Restaurant 2026. All Right Reserved",
+    },
+    notFoundPage: {
+        title: "404 Page not found !",
+        callToAction: "Go back home",
+        description: "Page not found.",
+    },
 };
