678592c25b25616d066dfbd5833ed45525eb68c6
trab <btraore25@yahoo.com>
2026-04-06 19:21:29 +0200

    Specify project base (including code repository name) in the conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-            "vmdi": path.resolve(__dirname, "node_modules/vue-material-design-icons"),
-        }
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        base: `/${env.VITE_REPO_NAME}/`,
+        plugins: [vue(), tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+                vmdi: path.resolve(
+                    __dirname,
+                    "node_modules/vue-material-design-icons",
+                ),
+            },
+        },
+    };
 });
